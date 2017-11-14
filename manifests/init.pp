2d9ee56549e83c8c5ce5df973ae3c6cfe6a8423f
Jarret Lavallee <jarret.lavallee@puppet.com>
2017-11-14 17:31:27 +0800

    Initial module commit
    Prior to this commit there was no module. this commit adds a base module
    using the pdk

---
+++ manifests/init.pp
@@ -0,0 +1,11 @@
+# brownbag
+#
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include brownbag
+class brownbag {
+  notify { 'This is the brownbag class': }
+}
